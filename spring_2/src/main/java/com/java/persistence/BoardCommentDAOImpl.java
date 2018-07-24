a8e71c87920873998d63ee0e322a1c1c2ee060b5
Leehwasub <Leehwasub@112.173.96.247>
2018-07-24 16:45:03 +0900

    커밋

--- spring_2/src/main/java/com/java/persistence/BoardCommentDAOImpl.java
+++ spring_2/src/main/java/com/java/persistence/BoardCommentDAOImpl.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 
 import com.java.domain.BoardCommentVO;
 
+@Repository
 public class BoardCommentDAOImpl implements BoardCommentDAO {
 	
 	@Inject
